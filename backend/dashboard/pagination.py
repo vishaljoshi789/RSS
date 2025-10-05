e2fe06dc635e6bd1f64108fd5115ea2965990c86
Vishal Joshi <vishalwebdev0987@gmail.com>
2025-10-05 17:56:23 +0530

    removed max page size limit from pagination

--- backend/dashboard/pagination.py
+++ backend/dashboard/pagination.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 30
     page_size_query_param = 'page_size'
-    max_page_size = 100
 
     def get_paginated_response(self, data):
         return Response({
