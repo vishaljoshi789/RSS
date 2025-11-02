67e54793af0f77eb1da92c2a477007602f750704
Pawandasila <pawandasila06@gmail.com>
2025-11-02 11:35:55 +0530

    updated sidebar

--- frontend/src/app/(dashboard)/dashboard/_components/nav-config.ts
+++ frontend/src/app/(dashboard)/dashboard/_components/nav-config.ts
@@ -83,13 +83,13 @@ export const DASHBOARD_NAV_ITEMS: NavItemConfig[] = [
     description: "User management",
     roles: ["admin", "staff"],
   },
-  {
-    title: "Events",
-    url: "/dashboard/events",
-    icon: Calendar,
-    description: "Event planning",
-    roles: ["admin", "staff", "volunteer"],
-  },
+  // {
+  //   title: "Events",
+  //   url: "/dashboard/events",
+  //   icon: Calendar,
+  //   description: "Event planning",
+  //   roles: ["staff", "volunteer"],
+  // },
   {
     title: "Vayapari",
     url: "/dashboard/vyapari",
@@ -116,7 +116,7 @@ export const DASHBOARD_NAV_ITEMS: NavItemConfig[] = [
     url: "/dashboard/id-card-management",
     icon: IdCard,
     description: "Manage ID cards",
-    roles: ["member"],
+    roles: ["member", "volunteer"],
   },
   {
     title: "Referral",
@@ -130,7 +130,7 @@ export const DASHBOARD_NAV_ITEMS: NavItemConfig[] = [
     url: "/dashboard/volunteer-registration",
     icon: CreditCard,
     description: "Volunteer Registration",
-    roles: ["volunteer", "admin"],
+    roles: ["volunteer"],
   },
   {
     title: "Volunteer Details",
